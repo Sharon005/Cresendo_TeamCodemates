8e391d0da30f1c0d65e7702869e06304a2f96c60
[Sharon005] <[sharonts123@gmail.com]>
2021-03-20 13:37:00 +0530

    Adding urls.py

---
+++ E_Darji/store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+        #Leave as empty string for base url
+	path('', views.home, name="home"),
+	path('add/', views.add, name="add"),
+	
+
+]
--- E_Darji/store/views.py
+++ E_Darji/store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def add(request):
+    context = {}
+    return render(request, 'store/add.html', context)
+
+
+def home(request):
+    context = {}
+    return render(request, 'store/home.html', context)
+
+
+
+
